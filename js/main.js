049ba341344ec91b63cac142e2ad0623e11499ad
Abdullah Chaudhry <19255312+AbdullahChaudhry@users.noreply.github.com>
2020-10-25 13:01:58 +0000

    fix prayer times

--- js/main.js
+++ js/main.js
@@ -1,117 +1,97 @@
-$(document).ready(function() {
-    // get prayer times
-    $.getJSON('https://muslimsalat.com/daily.json?key=48ac1b295a79364e5197c342429ab6e6&method=4&jsoncallback=?', setPrayerTimes)
+$(document).ready(function () {
+  $.getJSON("http://api.aladhan.com/v1/timingsByCity?city=London&country=United Kingdom&method=8", setPrayerTimes);
 
-    // get islamic calander date
-    $.getJSON('https://api.aladhan.com/gToH?', setDate);
-
-    // get a random wallpaper
-    getWallpaper();
+  setWallpaper();
 });
 
-function getWallpaper() {
+function setWallpaper() {
   var rand = Math.floor(Math.random() * 9);
-
   var mosque = mosques[rand];
 
-  // set the background image
   document.body.style.backgroundImage = "url(" + mosque.path + ")";
-
-  // info box
-  // var info = document.getElementById('info');
-  // info.innerText = mosque.country;
-
-  // debugging
-  console.log("url(" + mosques[rand].path + ")");
 }
-  //data
-  var mosques = [
+
+var mosques = [
   {
-      path: 'img/mosque1.jpg',
-      name: 'test',
-      country: 'test'
+    path: "img/mosque1.jpg",
+    name: "test",
+    country: "test",
   },
   {
-      path: 'img/mosque2.jpg',
-      name: 'test',
-      country: 'test'
+    path: "img/mosque2.jpg",
+    name: "test",
+    country: "test",
   },
   {
-      path: 'img/mosque3.jpg',
-      name: 'test',
-      country: 'test'
+    path: "img/mosque3.jpg",
+    name: "test",
+    country: "test",
   },
   {
-      path: 'img/mosque4.jpg',
-      name: 'test',
-      country: 'test'
+    path: "img/mosque4.jpg",
+    name: "test",
+    country: "test",
   },
   {
-      path: 'img/mosque5.jpg',
-      name: 'test',
-      country: 'test'
+    path: "img/mosque5.jpg",
+    name: "test",
+    country: "test",
   },
   {
-      path: 'img/mosque6.jpg',
-      name: 'test',
-      country: 'test'
+    path: "img/mosque6.jpg",
+    name: "test",
+    country: "test",
   },
   {
-      path: 'img/mosque7.jpg',
-      name: 'test',
-      country: 'test'
+    path: "img/mosque7.jpg",
+    name: "test",
+    country: "test",
   },
   {
-      path: 'img/mosque8.jpg',
-      name: 'test',
-      country: 'test'
+    path: "img/mosque8.jpg",
+    name: "test",
+    country: "test",
   },
   {
-      path: 'img/mosque9.jpg',
-      name: 'test',
-      country: 'Cairo'
-  }
-  ];
+    path: "img/mosque9.jpg",
+    name: "test",
+    country: "Cairo",
+  },
+];
 
-  function setDate(data) {
+function formatTime(t) {
+  t = t.replace(/:/, ".");
+  return t;
+}
 
-    data = data.data;
+function setPrayerTimes(data) {
+  let weekday = data.data.date.gregorian.weekday.en;
+  let day = data.data.date.gregorian.day
+  let month = data.data.date.gregorian.month.en
+  let year = data.data.date.gregorian.year
 
-    // gregorian calander date
-    var gregorianDate = document.getElementById('gregorianDate');
+  let hijriWeekday = data.data.date.hijri.day;
+  let hijriMonth = data.data.date.hijri.month.en;
+  let hijriYear = data.data.date.hijri.year;
 
-   // abbreviate weekday
-    var weekday = (data.gregorian.weekday.en);
+  hijriDate = `${hijriWeekday} ${hijriMonth} ${hijriYear}`
 
-    // set the gregorian date
-    gregorianDate.innerText = weekday + ', ' +  data.gregorian.day + ' ' + data.gregorian.month.en + ' ' + data.gregorian.year;
+  let gregoriandate = `${weekday}, ${day} ${month} ${year}`
+  times = data.data.timings;
 
-    // islamic calander date
-    var islamicDate = document.getElementById('islamicDate');
-    islamicDate.innerText = data.hijri.day + ' ' + data.hijri.month.en + ' ' + data.hijri.year;
-  }
+  var fajr = formatTime(times["Fajr"]);
+  var sunrise = formatTime(times["Sunrise"]);
+  var dhuhr = formatTime(times["Dhuhr"]);
+  var asr = formatTime(times["Asr"]);
+  var maghrib = formatTime(times["Maghrib"]);
+  var isha = formatTime(times["Isha"]);
 
-  // format the time
-  function timeFormat(t) {
-    t = t.replace(/:/, '.');
-    return t;
-  }
-
-  function setPrayerTimes(times) {
-    var location = times.city + ", " + times.country;
-    var fajr = timeFormat(times.items[0].fajr);
-    var shurooq = timeFormat(times.items[0].shurooq);
-    var dhuhr = timeFormat(times.items[0].dhuhr);
-    var asr = timeFormat(times.items[0].asr);
-    var maghrib = timeFormat(times.items[0].maghrib);
-    var isha = timeFormat(times.items[0].isha);
-
-    // set the prayer times to textbox
-    document.getElementById('location').innerText = location;
-    document.getElementById('fajr').innerText = fajr;
-    document.getElementById('sunrise').innerText = shurooq;
-    document.getElementById('dhuhr').innerText = dhuhr;
-    document.getElementById('asr').innerText = asr;
-    document.getElementById('maghrib').innerText = maghrib;
-    document.getElementById('isha').innerText = isha;
-  }
+  document.getElementById('gregorianDate').innerText = gregoriandate;
+  document.getElementById('hijriDate').innerText = hijriDate;
+  document.getElementById("fajr").innerText = fajr;
+  document.getElementById('sunrise').innerText = sunrise;
+  document.getElementById('dhuhr').innerText = dhuhr;
+  document.getElementById('asr').innerText = asr;
+  document.getElementById('maghrib').innerText = maghrib;
+  document.getElementById('isha').innerText = isha;
+}
