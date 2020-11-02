3c0a0a2e4fb6f9dd58d3bab344b22ab860c9ca04
Abdullah Chaudhry <19255312+AbdullahChaudhry@users.noreply.github.com>
2020-11-02 22:10:00 +0000

    add support for objects

--- src/utils/renderTemplate.ts
+++ src/utils/renderTemplate.ts
@@ -1,6 +1,10 @@
 export function renderTemplate(template: string, context: any) {
   return template.replace(/{{(.*)}}/g, function(x) {
-    let newStr = x.replace("{{", "").replace("}}", "")
-    return context[newStr]
+    let str = x.replace("{{", "").replace("}}", "")
+    if (str.includes(".")) {
+      return str.split(".").reduce((prev: any, next: any) => prev[next], context)
+    } else {
+      return context[str]
+    }
   })
 }
