e96e6d4962e97de9442bb394fa3156bb815120f4
Abdullah Chaudhry <19255312+AbdullahChaudhry@users.noreply.github.com>
2020-11-02 00:22:14 +0000

    add utils

---
+++ src/utils/time.ts
@@ -0,0 +1,21 @@
+export function toTwelveHour(time: any): string {
+  // Check correct time format and split into components
+  time = time.toString().match(/^([01]\d|2[0-3])(:)([0-5]\d)(:[0-5]\d)?$/) || [
+    time,
+  ];
+
+  if (time.length > 1) {
+    // If time format correct
+    time = time.slice(1); // Remove full string match value
+    time[0] = +time[0] % 12 || 12; // Adjust hours
+  }
+  return time.join("");
+}
+
+export function toPeriod(time: string): string {
+  return time.replace(/:/, ".");
+}
+
+export function formatTime(time: string): string {
+  return toPeriod(toTwelveHour(time));
+}
