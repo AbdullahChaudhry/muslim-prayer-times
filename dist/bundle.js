58ebbf985f6035822ecf0b0f8bfe7a1810b5ec5f
Abdullah Chaudhry <19255312+AbdullahChaudhry@users.noreply.github.com>
2020-11-04 00:31:21 +0000

    update

--- dist/bundle.js
+++ dist/bundle.js
@@ -8,6 +8,180 @@
   function __metadata(metadataKey, metadataValue) {
     if ("object" === typeof Reflect && "function" === typeof Reflect.metadata) return Reflect.metadata(metadataKey, metadataValue);
   }
+  function __awaiter(thisArg, _arguments, P, generator) {
+    function adopt(value) {
+      return value instanceof P ? value : new P((function(resolve) {
+        resolve(value);
+      }));
+    }
+    return new (P || (P = Promise))((function(resolve, reject) {
+      function fulfilled(value) {
+        try {
+          step(generator.next(value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function rejected(value) {
+        try {
+          step(generator["throw"](value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function step(result) {
+        result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected);
+      }
+      step((generator = generator.apply(thisArg, _arguments || [])).next());
+    }));
+  }
+  function __generator(thisArg, body) {
+    var _ = {
+      label: 0,
+      sent: function() {
+        if (1 & t[0]) throw t[1];
+        return t[1];
+      },
+      trys: [],
+      ops: []
+    }, f, y, t, g;
+    return g = {
+      next: verb(0),
+      throw: verb(1),
+      return: verb(2)
+    }, "function" === typeof Symbol && (g[Symbol.iterator] = function() {
+      return this;
+    }), g;
+    function verb(n) {
+      return function(v) {
+        return step([ n, v ]);
+      };
+    }
+    function step(op) {
+      if (f) throw new TypeError("Generator is already executing.");
+      while (_) try {
+        if (f = 1, y && (t = 2 & op[0] ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 
+        0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+        if (y = 0, t) op = [ 2 & op[0], t.value ];
+        switch (op[0]) {
+         case 0:
+         case 1:
+          t = op;
+          break;
+
+         case 4:
+          _.label++;
+          return {
+            value: op[1],
+            done: false
+          };
+
+         case 5:
+          _.label++;
+          y = op[1];
+          op = [ 0 ];
+          continue;
+
+         case 7:
+          op = _.ops.pop();
+          _.trys.pop();
+          continue;
+
+         default:
+          if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (6 === op[0] || 2 === op[0])) {
+            _ = 0;
+            continue;
+          }
+          if (3 === op[0] && (!t || op[1] > t[0] && op[1] < t[3])) {
+            _.label = op[1];
+            break;
+          }
+          if (6 === op[0] && _.label < t[1]) {
+            _.label = t[1];
+            t = op;
+            break;
+          }
+          if (t && _.label < t[2]) {
+            _.label = t[2];
+            _.ops.push(op);
+            break;
+          }
+          if (t[2]) _.ops.pop();
+          _.trys.pop();
+          continue;
+        }
+        op = body.call(thisArg, _);
+      } catch (e) {
+        op = [ 6, e ];
+        y = 0;
+      } finally {
+        f = t = 0;
+      }
+      if (5 & op[0]) throw op[1];
+      return {
+        value: op[0] ? op[1] : void 0,
+        done: true
+      };
+    }
+  }
+  function Component(options) {
+    return function decorator(cls) {
+      Reflect.defineMetadata("component", options, cls);
+    };
+  }
+  function toTwelveHour(time) {
+    // Check correct time format and split into components
+    time = time.toString().match(/^([01]\d|2[0-3])(:)([0-5]\d)(:[0-5]\d)?$/) || [ time ];
+    if (time.length > 1) {
+      // If time format correct
+      time = time.slice(1);
+ // Remove full string match value
+            time[0] = +time[0] % 12 || 12;
+ // Adjust hours
+        }
+    return time.join("");
+  }
+  function toPeriod(time) {
+    return time.replace(/:/, ".");
+  }
+  function formatTime(time) {
+    return toPeriod(toTwelveHour(time));
+  }
+  function compileTemplate(template, context) {
+    return template.replace(/{{(.*)}}/g, (function(x) {
+      var str = x.replace("{{", "").replace("}}", "");
+      if (str.includes(".")) return str.split(".").reduce((function(prev, next) {
+        return prev[next];
+      }), context); else return context[str];
+    }));
+  }
+  function render(instance) {
+    var metadata = Reflect.getMetadata("component", instance.constructor);
+    var selector = metadata.selector;
+    var template = metadata.template;
+    var HTMLMarkup = compileTemplate(template, instance);
+    var appElem = document.getElementById(selector);
+    appElem.innerHTML = HTMLMarkup;
+    return Promise.resolve(1);
+  }
+  function platformBrowserDynamic() {
+    function bootstrapComponent(component) {
+      function initComponent(component) {
+        var instance = new component;
+        render(instance).then((function() {
+          instance.init().then((function() {
+            render(instance);
+          }));
+        }));
+      }
+      document.onreadystatechange = function() {
+        if ("complete" === document.readyState) initComponent(component);
+      };
+    }
+    return {
+      bootstrapComponent: bootstrapComponent
+    };
+  }
   var endpoint = "https://api.aladhan.com/v1/timingsByCity";
   var country = "United Kingdom";
   var city = "London";
@@ -41,37 +215,6 @@
     };
     return WallpaperService;
   }();
-  function toTwelveHour(time) {
-    // Check correct time format and split into components
-    time = time.toString().match(/^([01]\d|2[0-3])(:)([0-5]\d)(:[0-5]\d)?$/) || [ time ];
-    if (time.length > 1) {
-      // If time format correct
-      time = time.slice(1);
- // Remove full string match value
-            time[0] = +time[0] % 12 || 12;
- // Adjust hours
-        }
-    return time.join("");
-  }
-  function toPeriod(time) {
-    return time.replace(/:/, ".");
-  }
-  function formatTime(time) {
-    return toPeriod(toTwelveHour(time));
-  }
-  function renderTemplate(template, context) {
-    return template.replace(/{{(.*)}}/g, (function(x) {
-      var str = x.replace("{{", "").replace("}}", "");
-      if (str.includes(".")) return str.split(".").reduce((function(prev, next) {
-        return prev[next];
-      }), context); else return context[str];
-    }));
-  }
-  function Component(options) {
-    return function decorator(klass) {
-      Reflect.defineMetadata("component", options, klass);
-    };
-  }
   var Reflect$1;
   (function(Reflect) {
     // Metadata Proposal
@@ -846,49 +989,62 @@
     function AppComponent(wallpaperService, prayerTimesService) {
       if (void 0 === wallpaperService) wallpaperService = new WallpaperService;
       if (void 0 === prayerTimesService) prayerTimesService = new PrayerTimesService;
-      this.currentWallpaper = null;
-      this.gregorianDate = null;
-      this.hijriDate = null;
-      this.fajr = null;
-      this.sunrise = null;
-      this.dhuhr = null;
-      this.asr = null;
-      this.maghrib = null;
-      this.isha = null;
+      this.currentWallpaper = "-";
+      this.gregorianDate = "-";
+      this.hijriDate = "-";
+      this.fajr = "-";
+      this.sunrise = "-";
+      this.dhuhr = "-";
+      this.asr = "-";
+      this.maghrib = "-";
+      this.isha = "-";
       this.wallpaperService = wallpaperService;
       this.prayerTimesService = prayerTimesService;
     }
-    AppComponent.prototype.init = function() {
-      var _this = this;
-      console.log(Reflect);
-      var wallpaperPromise = this.wallpaperService.getRandom().then((function(path) {
-        _this.currentWallpaper = path;
-        _this.setWallpaper(path);
-      }));
-      var prayerTimesPromise = this.prayerTimesService.getPrayerTimes().then((function(data) {
-        var gregorian = data.date.gregorian;
-        var hijri = data.date.hijri;
-        var times = data.timings;
-        var weekday = gregorian.weekday.en;
-        var day = gregorian.day;
-        var month = gregorian.month.en;
-        var year = gregorian.year;
-        var hijriWeekday = hijri.day;
-        var hijriMonth = hijri.month.en;
-        var hijriYear = hijri.year;
-        _this.gregorianDate = weekday + ", " + day + " " + month + " " + year;
-        _this.hijriDate = hijriWeekday + " " + hijriMonth + " " + hijriYear;
-        _this.fajr = formatTime(times["Fajr" /* Fajr */ ]);
-        _this.sunrise = formatTime(times["Sunrise" /* Sunrise */ ]);
-        _this.dhuhr = formatTime(times["Dhuhr" /* Dhuhr */ ]);
-        _this.asr = formatTime(times["Asr" /* Asr */ ]);
-        _this.maghrib = formatTime(times["Maghrib" /* Maghrib */ ]);
-        _this.isha = formatTime(times["Isha" /* Isha */ ]);
-      }));
-      return Promise.all([ wallpaperPromise, prayerTimesPromise ]);
-    };
     AppComponent.prototype.setWallpaper = function(path) {
       document.body.style.backgroundImage = "url(" + path + ")";
+    };
+    AppComponent.prototype.init = function() {
+      return __awaiter(this, void 0, void 0, (function() {
+        var _this = this;
+        return __generator(this, (function(_a) {
+          switch (_a.label) {
+           case 0:
+            console.log(Reflect);
+            return [ 4 /*yield*/ , this.wallpaperService.getRandom().then((function(path) {
+              _this.currentWallpaper = path;
+              _this.setWallpaper(path);
+            })) ];
+
+           case 1:
+            _a.sent();
+            return [ 4 /*yield*/ , this.prayerTimesService.getPrayerTimes().then((function(data) {
+              var gregorian = data.date.gregorian;
+              var hijri = data.date.hijri;
+              var times = data.timings;
+              var weekday = gregorian.weekday.en;
+              var day = gregorian.day;
+              var month = gregorian.month.en;
+              var year = gregorian.year;
+              var hijriWeekday = hijri.day;
+              var hijriMonth = hijri.month.en;
+              var hijriYear = hijri.year;
+              _this.gregorianDate = weekday + ", " + day + " " + month + " " + year;
+              _this.hijriDate = hijriWeekday + " " + hijriMonth + " " + hijriYear;
+              _this.fajr = formatTime(times["Fajr" /* Fajr */ ]);
+              _this.sunrise = formatTime(times["Sunrise" /* Sunrise */ ]);
+              _this.dhuhr = formatTime(times["Dhuhr" /* Dhuhr */ ]);
+              _this.asr = formatTime(times["Asr" /* Asr */ ]);
+              _this.maghrib = formatTime(times["Maghrib" /* Maghrib */ ]);
+              _this.isha = formatTime(times["Isha" /* Isha */ ]);
+            })) ];
+
+           case 2:
+            _a.sent();
+            return [ 2 /*return*/ ];
+          }
+        }));
+      }));
     };
     AppComponent = __decorate([ Component({
       selector: "app",
@@ -896,24 +1052,5 @@
     }), __metadata("design:paramtypes", [ Object, Object ]) ], AppComponent);
     return AppComponent;
   }();
-  function render(instance) {
-    var metadata = Reflect.getMetadata("component", AppComponent);
-    var selector = metadata.selector;
-    var template = metadata.template;
-    var HTMLMarkup = renderTemplate(template, instance);
-    var appElem = document.getElementById(selector);
-    appElem.innerHTML = HTMLMarkup;
-  }
-  function initComponent(component) {
-    var instance = new component;
-    instance.init().then((function() {
-      return render(instance);
-    }));
-  }
-  function init() {
-    document.onreadystatechange = function() {
-      if ("complete" === document.readyState) initComponent(AppComponent);
-    };
-  }
-  init();
+  platformBrowserDynamic().bootstrapComponent(AppComponent);
 })();
